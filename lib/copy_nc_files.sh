e77d1d822ce7c349b182ca98a3134d44d3afe96e
mirkodandrea <mirko.dandrea@gmail.com>
2025-07-03 14:18:46 +0200

    remove -T options to rsync

--- lib/copy_nc_files.sh
+++ lib/copy_nc_files.sh
@@ -7,4 +7,4 @@ declare -x RUNDATEDIR=${RUNDATE:0:4}/${RUNDATE:4:2}/${RUNDATE:6:2}/${RUNDATE:8:4
 declare -x NCDFPATH=$DDS_OUTPUT_PATH/$MODEL/netcdf/$RUNDATEDIR/
 
 mkdir -p $NCDFPATH
-rsync -av -T /run/user/1001/ /opt/risico/$MODEL/OUTPUT-NC/* $NCDFPATH
+rsync -av /opt/risico/$MODEL/OUTPUT-NC/* $NCDFPATH
