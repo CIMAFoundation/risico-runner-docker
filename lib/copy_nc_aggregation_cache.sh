70ffb473afee1b4a0837edb4490365a20adcd5a4
mirkodandrea <mirko.dandrea@gmail.com>
2025-07-03 17:37:57 +0200

    fix copy files

--- lib/copy_nc_aggregation_cache.sh
+++ lib/copy_nc_aggregation_cache.sh
@@ -6,5 +6,18 @@ CACHEDIR=$3
 declare -x RUNDATEDIR=${RUNDATE:0:4}/${RUNDATE:4:2}/${RUNDATE:6:2}/${RUNDATE:8:4}
 declare -x NCDFCACHEPATH=$DDS_OUTPUT_PATH/$MODEL/netcdf/$RUNDATEDIR/CACHE/
 
-mkdir -p $NCDFCACHEPATH
+echo "Copying netcdf aggregation cache for model $MODEL to $NCDFCACHEPATH"
+# check if the model directory exists
+if [ ! -d "$CACHEDIR" ]; then
+    echo "Cache directory $CACHEDIR does not exist."
+    exit 1
+fi
+
+# check if the output directory exists
+if [ ! -d "$NCDFCACHEPATH" ]; then
+    echo "Output cache directory $NCDFCACHEPATH does not exist. Creating it."
+    mkdir -p $NCDFCACHEPATH
+fi
+# copy netcdf aggregation cache files
+echo "Copying netcdf aggregation cache files from $CACHEDIR to $NCDFCACHEPATH"
 rsync -av $CACHEDIR $NCDFCACHEPATH
--- lib/copy_nc_files.sh
+++ lib/copy_nc_files.sh
@@ -6,5 +6,21 @@ declare -x RISICOPATH=/opt/risico/$MODEL
 declare -x RUNDATEDIR=${RUNDATE:0:4}/${RUNDATE:4:2}/${RUNDATE:6:2}/${RUNDATE:8:4}
 declare -x NCDFPATH=$DDS_OUTPUT_PATH/$MODEL/netcdf/$RUNDATEDIR/
 
-mkdir -p $NCDFPATH
-rsync -av /opt/risico/$MODEL/OUTPUT-NC/* $NCDFPATH
+echo "Copying netcdf files for model $MODEL to $NCDFPATH"
+
+# check if the model directory exists
+if [ ! -d "$RISICOPATH/OUTPUT-NC" ]; then
+    echo "Model output directory $RISICOPATH/OUTPUT-NC does not exist."
+    exit 1
+fi
+
+# check if the output directory exists
+if [ ! -d "$NCDFPATH" ]; then
+    echo "Output directory $NCDFPATH does not exist. Creating it."
+    mkdir -p $NCDFPATH
+fi
+
+# copy netcdf files
+echo "Copying netcdf files from $RISICOPATH/OUTPUT-NC to $NCDFPATH"
+
+rsync -av $RISICOPATH/OUTPUT-NC/* $NCDFPATH
